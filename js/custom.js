e1fc516c65ff32efa2d06588e24e40cd5a7d8380
Anny Hsu <annnnnyjj@gmail.com>
2016-03-12 19:29:02 +0000

    Change case study, how it works, timeline sections. Add new FAQ.

--- js/custom.js
+++ js/custom.js
@@ -282,67 +282,67 @@ jQuery(document).ready(function($) {
 /*---------------------------------------*/
 /*	TIMELINE SLIDER
 /*---------------------------------------*/
-jQuery(window).load(function() {
+// jQuery(window).load(function() {
 
-    'use strict';
+//     'use strict';
     
-    var x = 0,
-        init,
-        container = $('.timeline-section'),
-        /* TIMELINE SELECTOR */
-        items = container.find('li'),
-        containerHeight = 0,
-        numberVisible = 4,
-        /* NUMBER OF <li> TO SHOW IN SCROLLER */
-        intervalSec = 4000; /* INTERVAL TIME */
+//     var x = 0,
+//         init,
+//         container = $('.timeline-section'),
+//         /* TIMELINE SELECTOR */
+//         items = container.find('li'),
+//         containerHeight = 0,
+//         numberVisible = 4,
+//         /* NUMBER OF <li> TO SHOW IN SCROLLER */
+//         intervalSec = 4000; /* INTERVAL TIME */
 
-    if (!container.find('li:first').hasClass("first")) {
-        container.find('li:first').addClass("first");
-    }
+//     if (!container.find('li:first').hasClass("first")) {
+//         container.find('li:first').addClass("first");
+//     }
 
-    items.each(function() {
-        if (x < numberVisible) {
-            containerHeight = containerHeight + $(this).outerHeight();
-            x = x + 1;
-        }
-    });
+//     items.each(function() {
+//         if (x < numberVisible) {
+//             containerHeight = containerHeight + $(this).outerHeight();
+//             x = x + 1;
+//         }
+//     });
 
-    container.css({
-        height: containerHeight,
-        overflow: "hidden"
-    });
+//     container.css({
+//         height: containerHeight,
+//         overflow: "hidden"
+//     });
 
-    function vertCycle() {
-        var firstItem = container.find('li.first').html();
+//     function vertCycle() {
+//         var firstItem = container.find('li.first').html();
 
-        container.append('<li>' + firstItem + '</li>');
-        firstItem = '';
-        container.find('li.first').animate({
-            marginTop: "-105px",
-            opacity: "0"
-        }, 600, function() {
-            $(this).remove();
-            container.find('li:first').addClass("first");
-        });
-    }
+//         container.append('<li>' + firstItem + '</li>');
+//         firstItem = '';
+//         container.find('li.first').animate({
+//             marginTop: "-105px",
+//             opacity: "0"
+//         }, 600, function() {
+//             $(this).remove();
+//             container.find('li:first').addClass("first");
+//         });
+//     }
 
-    if (intervalSec < 700) {
-        intervalSec = 700;
-    }
+//     if (intervalSec < 700) {
+//         intervalSec = 700;
+//     }
 
-    init = setInterval(function() {
-        vertCycle();
-    }, intervalSec);
+//     init = setInterval(function() {
+//         vertCycle();
+//     }, intervalSec);
 
-    container.hover(function() {
-        clearInterval(init);
-    }, function() {
-        init = setInterval(function() {
-            vertCycle();
-        }, intervalSec);
-    });
+//     container.hover(function() {
+//         clearInterval(init);
+//     }, function() {
+//         init = setInterval(function() {
+//             vertCycle();
+//         }, intervalSec);
+//     });
 
-});
+// });
 
 /*---------------------------------------*/
 /*  VIDEO MODAL (FANCYBOX)
